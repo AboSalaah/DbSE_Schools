ec84d17da13eb4f5ad762466bc47c2230293de1c
MuhammadHamada <homshamada@gmail.com>
2017-04-25 13:57:29 +0200

    Add Fragments instead of activities

--- app/src/main/java/com/example/ahmedsaleh/dbse_schools/MainActivity.java
+++ app/src/main/java/com/example/ahmedsaleh/dbse_schools/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.ahmedsaleh.dbse_schools;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +20,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import static com.example.ahmedsaleh.dbse_schools.SignIn.openBbSE_FacebookPage;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +34,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +43,10 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.content_main, new HomeFragement());
+        ft.commit();
     }
 
     @Override
@@ -70,8 +77,45 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.action_settings) {
             return true;
         }
+        if(id == R.id.action_logout){
+            movToSignInActivity();
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void displaySelectedScreen(int id) {
+
+        Fragment fragment = null;
+        switch (id) {
+            case R.id.Home_icon:
+                fragment = new HomeFragement();
+                break;
+            case R.id.View_Profile:
+                fragment = new ViewProfileFragement();
+                break;
+            case R.id.Edit_profile:
+                fragment = new EditProfileFragement();
+                break;
+            case R.id.about_dbse:
+                fragment = new AboutDbseFragement();
+                break;
+            case R.id.Contacts:
+                fragment = new HomeFragement();
+                viewContactsDialog();
+                break;
+            case R.id.Sign_out:
+                movToSignInActivity();
+                break;
+        }
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_main, fragment);
+            ft.commit();
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -80,22 +124,48 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
+        displaySelectedScreen(id);
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    void viewContactsDialog(){
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+        final View mview=getLayoutInflater().inflate(R.layout.contacts_dialog,null);
+        mBuilder.setTitle(R.string.contacts_text);
+        mBuilder.setView(mview);
+        mBuilder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+            }
+        });
+        AlertDialog dialog=mBuilder.create();
+        dialog.show();
+
+        ImageView facbookpage = (ImageView) mview.findViewById(R.id.Facebook_dialog);
+        facbookpage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent facebookIntent = openBbSE_FacebookPage(MainActivity.this);
+                Intent chooser = Intent.createChooser(facebookIntent,"Open By");
+                startActivity(chooser);
+            }
+        });
+        TextView DbSE = (TextView) mview.findViewById(R.id.Dbse_dialog);
+        DbSE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent DbSE_Intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.dbse.co/schools"));
+                Intent chooser = Intent.createChooser(DbSE_Intent,"Open By");
+                startActivity(chooser);
+            }
+        });
+    }
+
+    private void movToSignInActivity() {
+        Intent i = new Intent(getApplicationContext(),SignIn.class);
+        startActivity(i);
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/ahmedsaleh/dbse_schools/SignIn.java
@@ -0,0 +1,282 @@
+package com.example.ahmedsaleh.dbse_schools;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.util.DiffUtil;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.ahmedsaleh.dbse_schools.MainActivity;
+import com.example.ahmedsaleh.dbse_schools.R;
+import com.example.ahmedsaleh.dbse_schools.SignUp;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+import static android.R.attr.data;
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
+
+public class SignIn extends AppCompatActivity {
+
+    EditText userOrEmail;
+    EditText password;
+    Button signInButton;
+    EditText emailForgetPassword;
+    TextView forgotPassword;
+    TextView signUpTextView;
+    ImageView facebookPage;
+    String result=null;
+    StringBuilder URL;
+    public static String token;
+    public static int id;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        userOrEmail = (EditText) findViewById(R.id.signIn_user_email);
+        password = (EditText) findViewById(R.id.signIn_password);
+        signInButton=(Button) findViewById(R.id.sign_in_button);
+        facebookPage =(ImageView) findViewById(R.id.Facebook_Button);
+        signUpTextView = (TextView) findViewById(R.id.SignIn_SignUp_text);
+        forgotPassword = (TextView) findViewById(R.id.forget_password) ;
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                URL = new StringBuilder(getString(R.string.url)+"signin");
+                if(validate()){
+//                    connect();
+                    moveToMainActivity();
+                }
+            }
+        });
+        facebookPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent facebookIntent = openBbSE_FacebookPage(SignIn.this);
+                Intent chooser = Intent.createChooser(facebookIntent,"Open By");
+                startActivity(chooser);
+            }
+        });
+        signUpTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moveToSignUpActivity();
+            }
+        });
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(SignIn.this);
+                final View mview=getLayoutInflater().inflate(R.layout.forgot_password_dialog,null);
+                mBuilder.setTitle(R.string.forget_pass_title1);
+                mBuilder.setPositiveButton(R.string.submit, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User clicked OK button
+                        URL = new StringBuilder(getString(R.string.url)+"forgetpassword");
+                        emailForgetPassword = (EditText) mview.findViewById(R.id.forget_password_code_editText);
+                        connectForgetPassword();
+                    }
+                });
+                mBuilder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+                mBuilder.setView(mview);
+                AlertDialog dialog=mBuilder.create();
+                dialog.show();
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_signUp) {
+            moveToSignUpActivity();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //  this function for validating the user input for signin
+    private boolean validate()
+    {
+        EditText email=(EditText)findViewById(R.id.signIn_user_email);
+        EditText password=(EditText)findViewById(R.id.signIn_password);
+        if(email.getText().toString().isEmpty())
+        {
+            email.setError("E-mail "+getString(R.string.emptyerror));
+            return false;
+        }
+        if(password.getText().toString().isEmpty())
+        {
+            password.setError("password "+getString(R.string.emptyerror));
+            return false;
+        }
+        return true;
+    }
+    private void moveToSignUpActivity(){
+        Intent i = new Intent(SignIn.this,SignUp.class);
+        startActivity(i);
+    }
+    private void moveToMainActivity(){
+        Intent i = new Intent(SignIn.this,MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+    public static Intent openBbSE_FacebookPage(Context context) {
+        try {
+            context.getPackageManager()
+                    .getPackageInfo("com.facebook.katana", 0);
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("fb://page/1220130878052358"));
+        } catch (Exception e){
+
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://www.facebook.com/pg/DatabaseSE.CO"));
+        }
+    }
+
+    void connectForgetPassword(){
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        Map<String, String> params = new HashMap<String, String>();
+        JSONObject parameter = new JSONObject(params);
+
+        params.put("email",emailForgetPassword.getText().toString());
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(JSON, parameter.toString());
+        Request request = new Request.Builder()
+                .url(URL.toString())
+                .post(body)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(okhttp3.Call call, IOException e) {
+//                Toast.makeText(SignIn.this,"Connection Failed", Toast.LENGTH_LONG).show();
+                Log.v("responsehhhhhhhhh", call.request().body().toString());
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(okhttp3.Call call, final Response response) throws IOException {
+                result = response.body().string().toString();
+                Log.v("Response code", String.valueOf(response.code()));
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            JSONObject json = new JSONObject(result);
+                            String msg = json.get("msg").toString();
+                            Toast.makeText(SignIn.this,msg, Toast.LENGTH_LONG).show();
+                        } catch (JSONException e) {
+                            Toast.makeText(SignIn.this,"The selected email is invalid!", Toast.LENGTH_LONG).show();
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+
+            }
+        });
+//        Toast.makeText(SignIn.this,"Connection Failed", Toast.LENGTH_LONG).show();
+    }
+
+    void connect()
+    {
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("login", String.valueOf(userOrEmail.getText()));
+        params.put("password", String.valueOf(password.getText()));
+        JSONObject parameter = new JSONObject(params);
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(JSON, parameter.toString());
+        Request request = new Request.Builder()
+                .url(URL.toString())
+                .post(body)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(okhttp3.Call call, IOException e) {
+                Log.v("responsehhhhhhhhh", call.request().body().toString());
+                e.printStackTrace();
+//                Toast.makeText(SignIn.this,"Connection Failed", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onResponse(okhttp3.Call call, final Response response) throws IOException {
+                result = response.body().string().toString();
+                Log.v("Response code", String.valueOf(response.code()));
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            JSONObject json = new JSONObject(result);
+                            String error = json.get("error").toString();
+//                            Toast.makeText(SignIn.this,error, Toast.LENGTH_LONG).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            try {
+                                JSONObject json = new JSONObject(result);
+                                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit()
+                                        .putString("token", json.getString("token")).commit();
+                                token = PreferenceManager.getDefaultSharedPreferences(getApplicationContext())
+                                        .getString("token", "defaultStringIfNothingFound");
+                                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit()
+                                        .putString("id", json.getString("id")).commit();
+                                id = Integer.parseInt(PreferenceManager.getDefaultSharedPreferences(getApplicationContext())
+                                        .getString("id", "defaultStringIfNothingFound"));
+                                moveToMainActivity();
+                            } catch (JSONException ex){
+                                ex.printStackTrace();
+                            }
+                        }
+
+                    }
+                });
+
+            }
+        });
+//        Toast.makeText(SingIn.this,"Connection Failed", Toast.LENGTH_LONG).show();
+    }
+
+
+}
